0c8235a6d64e261580bba2b1d917f1d80999e1fa
AlexisBrunel <97045305+AlexisBrunel@users.noreply.github.com>
2023-11-22 16:18:31 +0100

    Create goad_install.sh

---
+++ goad_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Cloner le dépôt Git
+git clone https://github.com/Orange-Cyberdefense/GOAD
+
+# Accéder au répertoire cloné
+cd GOAD
+
+# Exécuter la première commande
+./goad.sh -t check -l GOAD -p virtualbox -m docker
+
+# Exécuter la deuxième commande
+./goad.sh -t install -l GOAD -p virtualbox -m docker
